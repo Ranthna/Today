054b95a6728d9e792a308538b52a3f566b3c5c2b
Ranthna <154591864+Ranthna@users.noreply.github.com>
2023-12-23 21:19:55 +0700

    added hungry prompt

--- hungry.py
+++ hungry.py
@@ -1,1 +1,12 @@
 print("I'm hungry")
+num=[2,3,11,4,4,6]
+total=0
+for nums in num:
+  total =total+nums
+print(total)
+
+hungry=input("Are you hungry ?")
+if hungry=="yes":
+  print("You can eat all this food on the table.")
+else:
+  print("You can sit on the sofar wait me a min please")
